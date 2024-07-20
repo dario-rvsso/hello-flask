e525558b42e88ee49c4ed8d15fecdf5b3a719d50
dario-rvsso <58913887+dario-rvsso@users.noreply.github.com>
2024-07-21 00:14:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=80)
+#app.run(host='0.0.0.0', port=80)
 
 if __name__ == '__main__':
    app.run()
